6399f66d6289ed6a0d486c12db45b9aa93bc4616
amit <asj8139@g.rit.edu>
2018-01-27 19:35:12 -0500

    Done with basic intents

--- lambda/index.js
+++ lambda/index.js
@@ -10,7 +10,24 @@
 const Alexa = require('alexa-sdk');
 const appConstants = require('./constants.js');
 
-const folkTaleHandlers = {};
+const SKILL_NAME = 'Mavis';
+const HELP_MESSAGE = `Mavis tells you the story you want to hear. 
+  Suppose you want to listen to a fox story, just say "Tell me a fox story". 
+  Now try asking Mavis a story you would like to hear.`;
+const HELP_REPROMPT = "Try saying, tell me a fox story.";
+const STOP_MESSAGE = 'Goodbye!';
+
+const folkTaleHandlers = {
+    'AMAZON.HelpIntent': function () {
+        this.emit(":ask", HELP_MESSAGE, HELP_REPROMPT);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    }
+};
 
 // Handler for AWS lambda
 exports.handler = function (event, context, callback) {
@@ -19,4 +36,5 @@ exports.handler = function (event, context, callback) {
     alexa.dynamoDBTableName = appConstants.DYNAMODB_TABLE;
     alexa.registerHandlers(folkTaleHandlers);
     alexa.execute();
-  };
+};
+
